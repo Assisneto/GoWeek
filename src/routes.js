ffeb81c8f4a34f33ee27f1566b43c31fa38ef729
Assisneto <netodamasceno12@gmail.com>
2018-11-08 11:52:08 -0300

    likeControl

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,13 @@ const express = require('express');
 const routes = express.Router();
 
 const TweetController = require('./controllers/TweetControllers');
+const LikeControllers = require('./controllers/LikeController');
 
 routes.get('/', (req, res) => res.send('hellou world'));
 
 routes.get('/tweets', TweetController.index);
 routes.post('/tweets', TweetController.store);
+
+routes.put('/likes/:id', LikeControllers.store);
 
 module.exports = routes;
