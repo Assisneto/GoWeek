b186ca0d1299510178f29f1030f4887eabda512f
Assisneto <netodamasceno12@gmail.com>
2018-11-08 11:15:28 -0300

    Tweet Schema

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect(
+  'mongodb://assisneto:assismenina123@ds253713.mlab.com:53713/go-week-db',
+  {
+    useNewUrlParser: true,
+  },
+);
 
 app.get('/', (req, res) => res.send('hello world'));
 
