1d0aa8ab10883aa109acba886ea74f52288c5d7d
Assisneto <netodamasceno12@gmail.com>
2018-11-18 15:02:25 -0300

    component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 
+import Login from './pages/Login.js';
+import Timeline from './pages/Timeline.js';
 
 class App extends Component {
   render() {
     return (
-      <h1>hello world</h1>
+      <BrowserRouter>
+        <Switch>
+          <Route path ="/" exact component={Login} />
+          
+          <Route path ="/timeline" component={Timeline} />
+
+          
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+
+import './Login.css';
+import twitterLogo from "../twitter.svg";
+
+export default class Login extends Component {
+  
+  state = {
+    username : '',
+  };
+
+  handleSubmit = event =>{
+    event.preventDefault();
+    const {username} = this.state;
+
+    if(!username.length) return;
+
+    localStorage.setItem('@GoTwitter:username',username);
+
+    this.props.history.push('/timeline');
+  }
+
+  handleInputChange = event =>{
+    this.setState({username: event.target.value});
+
+
+  };
+  
+  render() {
+    return (
+      <div className="login-wrapper">
+        <img src = {twitterLogo} alt ="GoTwitter"/>
+        <form onSubmit = {this.handleSubmit}>
+          <input
+            value={this.state.username}
+            onChange ={this.handleInputChange}
+            placeholder="Nome de Usuário"
+           />
+          <button type ="submit">Entrar</button>
+        </form>
+      </div>
+
+    );
+  }
+}
